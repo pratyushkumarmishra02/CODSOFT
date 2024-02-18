a79192a11b4e76490faf776bafe980c4219dfb1d
PRATYUSH KUMAR MISHRA <146066300+pratyushkumarmishra02@users.noreply.github.com>
2024-02-18 22:52:01 +0530

    Update ContactBook.py

--- ContactBook.py
+++ ContactBook.py
@@ -1,9 +1,4 @@
-import random
-import string
-
-def generate_password(length):
-    characters = string.ascii_letters + string.digits + string.punctuation
-    pasclass ContactBook:
+class ContactBook:
     def __init__(self):
         self.contacts = {}
 
@@ -84,25 +79,6 @@ def main():
         else:
             print("Invalid choice. Please try again.")
 
-
-if __name__ == "__main__":
-    main()
-sword = ''.join(random.choice(characters) for _ in range(length))
-    return password
-
-def main():
-    print("Welcome to the Password Generator!")
-    while True:
-        try:
-            length = int(input("Enter the desired length of the password: "))
-            if length <= 0:
-                print("Length should be a positive integer.")
-                continue
-            password = generate_password(length)
-            print("Generated Password:", password)
-            break
-        except ValueError:
-            print("Invalid input. Please enter a valid integer.")
 
 if __name__ == "__main__":
     main()
